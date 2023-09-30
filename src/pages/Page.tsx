23c22f136e13690504523d73f421388044c73791
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-09-29 22:17:42 -0300

    Footer done

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import { Navbar } from '../components/Navbar/Navbar'
 import { Footer } from '../components/Footer/Footer';
+import "./Page.css"
 
 interface PageProps {
     children: ReactNode;
@@ -8,10 +9,12 @@ interface PageProps {
 
 export const Page: React.FC<PageProps> = ({ children }) => {
   return (
-    <div>
-        <Navbar/>
-        {children}
-        <Footer/>
+    <div className="page">
+      <Navbar />
+        <div className="content">
+          <main>{children}</main>
+        </div>
+      <Footer />
     </div>
   )
 }
