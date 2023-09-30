a43c97bf10f0a10b2d0f22fc70784f6fcb8d9d32
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-09-29 22:35:39 -0300

    making it more responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Page } from '../Page';
+import UMLDiagram from '../../assets/images/workshop-diagram.drawio.png'
 import './Home.css'
 
 const HomePage: React.FC = () => {
@@ -10,7 +11,7 @@ const HomePage: React.FC = () => {
         <p>This is a web-app build to test and learn about web development using Java Springboot, React.ts, PostgreSql</p>
         <p>The UML Diagram:</p>
         <div className="image-container">
-          <img src="src\assets\workshop-diagram.drawio.png" alt="UML Diagram of the project" />
+          <img src={UMLDiagram} alt="UML Diagram of the project" />
         </div>
       </div>
     </Page>
